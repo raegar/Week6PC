a89fb6334fd7959473b1009a51344b3e1ff60cc4
Jamie Myland <jamie.myland@gmail.com>
2020-10-29 15:12:02 +0000

    Added definition for MotorVehicle

--- IntroOOP/IntroOOP/MotorVehicle.cs
+++ IntroOOP/IntroOOP/MotorVehicle.cs
@@ -6,5 +6,51 @@ namespace IntroOOP
 {
     class MotorVehicle
     {
+        string make;
+        string model;
+        int speed = 0;
+        int gear = 0;
+
+        public MotorVehicle(string make, string model)
+        {
+            this.make = make;
+            this.model = model;
+        }
+
+        public void Accelerate()
+        {
+            speed += gear;
+        }
+
+        public void Brake()
+        {
+            if (speed > 0)
+            {
+                speed -= 1;
+            }
+        }
+
+        public void ChangeGearUp()
+        {
+            if (gear < 5)
+            {
+                gear++;
+            }
+        }
+
+        public void ChangeGearDown()
+        {
+            if (gear > 0)
+            {
+                gear--;
+            }
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Make & model: {make} {model}");
+            Console.WriteLine($"Current Speed: {speed}");
+            Console.WriteLine($"Current Gear: {gear}");
+        }
     }
 }
